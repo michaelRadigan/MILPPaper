c53bbb98d46e75b3893afadd6fe2eabe418600bd
Michael Radigan <michael.radigan14@imperial.ac.uk>
2020-01-06 23:27:42 +0000

    wget -> curl to work on OSX

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 # Download an unpack nauty
-wget -c http://users.cecs.anu.edu.au/~bdm/nauty/nauty26r12.tar.gz -O - | tar -xz
+curl http://users.cecs.anu.edu.au/~bdm/nauty/nauty26r12.tar.gz | tar -xz
 # grab the folder name TODO[michaelr]: Make this less magic
 nauty=`ls | grep nauty`
 (cd $nauty && ./configure CFLAGS='-O4 -fPIC' && make)
-wget -c https://web.cs.dal.ca/~peter/software/pynauty/pynauty-0.6.0.tar.gz -O - | tar -xz
+curl https://web.cs.dal.ca/~peter/software/pynauty/pynauty-0.6.0.tar.gz | tar -xz
 pynauty=`ls | grep pynauty`
 cd $pynauty
 x=../$nauty
