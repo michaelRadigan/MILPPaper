26c276373e411716171b66807f5b2d07527c46c7
Michael Radigan <michael.radigan14@imperial.ac.uk>
2020-01-12 16:41:49 +0000

    Remove unused, unneeded files

--- src/test.py
+++
@@ -1,6 +0,0 @@
-from symmetryFinder import *
-from simpleAutomorphismGroupCalculator import *
-
-s = SymmetryFinder(largerConstraintMatrix)
-weightedS = SymmetryFinder(largerWeightedConstraintMatrix)
-
