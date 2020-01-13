1071a7ba9f8533ff1ecc1f37afa6622d09250fb1
Michael Radigan <michael.radigan14@imperial.ac.uk>
2020-01-13 12:43:00 +0000

    Add test for weighted matrix

--- src/test/test_intermediateSymmetryFinder.py
+++ src/test/test_intermediateSymmetryFinder.py
@@ -5,6 +5,23 @@ from scipy.sparse import csr_matrix
 import intermediateSymmetryFinder as sf
 
 
+def constructMatrixOnlyLinProblem(A):
+    beq = np.array([[1], [1], [1], [1], [1], [1], [1]])
+
+    # TODO[michaelr]: Clearly this is wrong but just making it work with what we already have...
+    f = np.array([[1], [1], [1], [1], [1], [1]])
+    intcon = np.array([[1], [1], [1], [1], [1], [1]])
+    lb = np.array([[1], [1], [1], [1], [1], [1]])
+    ub = np.array([[1], [1], [1], [1], [1], [1]])
+
+    # TODO[michaelr]: Fx this! Passing in the eq to ineq to get around the awful design
+    return LinearProblem(A, A, beq, beq, f, intcon, lb, ub)
+
+
+def listToSparseMatrix(matrix):
+    return csr_matrix([np.array(x) for x in matrix]).tocoo()
+
+
 class TestIntermediateSymmetryFinder(TestCase):
     constraintMatrix = [
         [1, 1, 0, 0, 0, 0],
@@ -18,9 +35,9 @@ class TestIntermediateSymmetryFinder(TestCase):
 
     weightedConstraintMatrix = [
         [1, 1, 0, 0, 0, 0],
-        [1, 0, 1, 0, 0, 0],
+        [1, 0, 3, 0, 0, 0],
         [1, 0, 0, 1, 0, 0],
-        [0, 1, 0, 0, 1, 0],
+        [0, 1, 0, 0, 3, 0],
         [0, 1, 0, 0, 0, 1],
         [0, 0, 2, 2, 0, 0],
         [0, 0, 0, 0, 2, 2],
@@ -66,38 +83,22 @@ class TestIntermediateSymmetryFinder(TestCase):
 
     objectiveTest = [1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 1, 1]
 
+    # TODO[michaelr] Do we actually care about the orbits of the constraints?
+
     def test_simpleUnweightedMatrixOnly(self):
-        Aeq = csr_matrix([np.array(x) for x in self.constraintMatrix]).tocoo()
-        beq = np.array([[1], [1], [1], [1], [1], [1], [1]])
-
-
-        # TODO[michaelr]: Clearly this is wrong but just making it work with what we already have...
-        f = np.array([[1], [1], [1], [1], [1], [1]])
-        intcon = np.array([[1], [1], [1], [1], [1], [1]])
-        lb = np.array([[1], [1], [1], [1], [1], [1]])
-        ub = np.array([[1], [1], [1], [1], [1], [1]])
-
-        # TODO[michaelr]: Fx this! Passing in the eq to ineq to get around the awful design
-        linProblem = LinearProblem(Aeq, Aeq, beq, beq, f, intcon, lb, ub)
+        Aeq = listToSparseMatrix(self.constraintMatrix)
+        linProblem = constructMatrixOnlyLinProblem(Aeq)
         sym = sf.findSymmetries(linProblem)
-        print(sym)
         self.assertEqual(sym, [0, 0, 2, 2, 2, 2, 6, 7, 7, 7, 7, 11, 11])
+
+    def test_simpleWeightedMatrixOnly(self):
+        Aeq = listToSparseMatrix(self.weightedConstraintMatrix)
+        linProblem = constructMatrixOnlyLinProblem(Aeq)
+        sym = sf.findSymmetries(linProblem)
+        self.assertEqual(sym, [0, 0, 2, 3, 2, 3, 6, 7, 8, 7, 8, 11, 11, 13, 13, 15, 16, 15, 16])
 
 
 '''
-    def testSimpleUnweightedSymmetries(self):
-        s = SymmetryFinder(self.constraintMatrix)
-        symmetries = s.get_symmetries()
-        symmetries.sort()
-
-        self.assertEqual(symmetries, [{0, 1}, {2, 3, 4, 5}])
-
-    def testSimpleWeightedSymmetries(self):
-        s = SymmetryFinder(self.weightedConstraintMatrix)
-        symmetries = s.get_symmetries()
-        symmetries.sort()
-        self.assertEqual(True, False)
-        self.assertEqual(symmetries, [{0, 1}, {2, 3, 4, 5}])
 
     def testLargerUnweightedSymmetries(self):
         s = SymmetryFinder(self.largerConstraintMatrix)
